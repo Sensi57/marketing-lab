c84b5ff41f580267179700fd2db1cac3859f4dd3
Sensi <averus2018@mail.ru>
2026-01-07 20:55:12 +0500

    fix: test

---
+++ commitlint.config.cjs
@@ -0,0 +1,37 @@
+const matchTicket = /^(feat|fix|refactor)-\d+: .+$/;
+
+module.exports = {
+    parserPreset: {
+        parserOpts: {
+            headerPattern: new RegExp('^' + matchTicket.source + '$'),
+            headerCorrespondence: ['header'],
+        },
+    },
+
+    plugins: [
+        {
+            rules: {
+                'header-match-team-pattern': (parsed) => {
+                    const { header } = parsed;
+
+                    if (header.includes('Release')) {
+                        return [true, ''];
+                    }
+
+                    if (!matchTicket.test(header)) {
+                        return [
+                            false,
+                            'Коммит должен быть в формате: feat-123: message | fix-123: message | refactor-123: message',
+                        ];
+                    }
+
+                    return [true, ''];
+                },
+            },
+        },
+    ],
+
+    rules: {
+        'header-match-team-pattern': [2, 'always'],
+    },
+};
